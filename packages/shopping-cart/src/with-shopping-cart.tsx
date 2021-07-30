74ead91666f4c61b1416440c2f2909554acbab37
Payton Swick <payton@foolord.com>
2021-07-30 18:13:36 -0400

    Add setCartKey to props injected by withShoppingCart

--- packages/shopping-cart/src/with-shopping-cart.tsx
+++ packages/shopping-cart/src/with-shopping-cart.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import ShoppingCartContext from './shopping-cart-context';
 import useShoppingCart from './use-shopping-cart';
 
 export default function withShoppingCart< P >( Component: React.ComponentType< P > ) {
 	return function ShoppingCartWrapper( props: P ): JSX.Element {
-		const shoppingCartManager = useShoppingCart();
+		// Even though managerClient isn't used here this guard will provide a
+		// better error message than waiting for the one in useShoppingCart.
+		const managerClient = useContext( ShoppingCartContext );
+		if ( ! managerClient ) {
+			throw new Error( 'withShoppingCart must be used inside a ShoppingCartProvider' );
+		}
+
+		const [ cartKey, setCartKey ] = useState< string | undefined >();
+
+		const shoppingCartManager = useShoppingCart( cartKey );
 		return (
 			<Component
 				{ ...props }
 				shoppingCartManager={ shoppingCartManager }
+				setCartKey={ setCartKey }
 				cart={ shoppingCartManager.responseCart }
 			/>
 		);
